34d974141e6b71106de70f5d722e52e691d6a01b
Tim Malich <tim.malich@gmail.com>
2022-03-21 10:31:58 +0100

    Correct copy and paste error

--- src/main/java/de/tmalich/example/fiqlmybatis/mapper/UsersMapper.java
+++ src/main/java/de/tmalich/example/fiqlmybatis/mapper/UsersMapper.java
@@ -18,18 +18,18 @@ public interface UsersMapper {
     @Select("select * from public.users")
     @Results(value = {
             @Result(column = UserTable.COLUMN_ID, property = User.PROPERTY_ID, id = true),
-            @Result(column = UserTable.COLUMN_FIRST_NAME, property = User.PROPERTY_FIRST_NAME, id = true),
-            @Result(column = UserTable.COLUMN_LAST_NAME, property = User.PROPERTY_LAST_NAME, id = true),
-            @Result(column = UserTable.COLUMN_EMAIL_ADDRESS, property = User.PROPERTY_EMAIL_ADDRESS, id = true)
+            @Result(column = UserTable.COLUMN_FIRST_NAME, property = User.PROPERTY_FIRST_NAME),
+            @Result(column = UserTable.COLUMN_LAST_NAME, property = User.PROPERTY_LAST_NAME),
+            @Result(column = UserTable.COLUMN_EMAIL_ADDRESS, property = User.PROPERTY_EMAIL_ADDRESS)
     })
     List<User> findAll();
 
     @SelectProvider(type = SqlProviderAdapter.class, method = "select")
     @Results(value = {
             @Result(column = UserTable.COLUMN_ID, property = User.PROPERTY_ID, id = true),
-            @Result(column = UserTable.COLUMN_FIRST_NAME, property = User.PROPERTY_FIRST_NAME, id = true),
-            @Result(column = UserTable.COLUMN_LAST_NAME, property = User.PROPERTY_LAST_NAME, id = true),
-            @Result(column = UserTable.COLUMN_EMAIL_ADDRESS, property = User.PROPERTY_EMAIL_ADDRESS, id = true)
+            @Result(column = UserTable.COLUMN_FIRST_NAME, property = User.PROPERTY_FIRST_NAME),
+            @Result(column = UserTable.COLUMN_LAST_NAME, property = User.PROPERTY_LAST_NAME),
+            @Result(column = UserTable.COLUMN_EMAIL_ADDRESS, property = User.PROPERTY_EMAIL_ADDRESS)
     })
     List<User> findByFilter(SelectStatementProvider selectStatement);
 }
